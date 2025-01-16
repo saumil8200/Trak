59f9dbe3354010872585b47108febff5d906d76b
saumil8200 <saumil8200@gmail.com>
2025-01-16 17:24:38 +0530

    edit anime name

--- src/components/AnimeItem.jsx
+++ src/components/AnimeItem.jsx
@@ -1,31 +1,51 @@
 /* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
 import { useAnime } from "../contexts/AnimeContext";
+import { useState } from "react";
 
 function AnimeItem({ anime }) {
-  const { deleteAnime } = useAnime();
+  const { updateAnime, deleteAnime } = useAnime();
+
+  const [animeName, setAnimeName] = useState(anime.anime);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleUpdateAnime = () => {
+    updateAnime(anime.id, { ...anime, anime: animeName });
+    setIsEditing(false);
+	};
 
   const handleDelete = () => {
     deleteAnime(anime.id);
   };
 
-  const handleEdit = () => {
-    console.log('Edit anime:', anime.id);
-  };
-
   return (
     <div className="flex justify-between items-center p-4 bg-white mb-2">
       <div>
-        <Link to={`/anime/${anime.id}`} className="text-xl font-bold">
-          {anime.anime}
-        </Link>
+        {isEditing ? (
+          <input
+            type="text"
+            value={animeName}
+            onChange={(e) => setAnimeName(e.target.value)} // Update anime name state
+            className="text-xl font-bold border-b-2 border-gray-300"
+          />
+        ) : (
+          <Link to={`/anime/${anime.id}`} className="text-xl font-bold">
+            {anime.anime}
+          </Link>
+        )}
         <p className="text-gray-500">{anime.progress}</p>
       </div>
 
       <div className="flex space-x-2">
-        <button onClick={handleEdit} className="bg-gray-950 text-white px-4 py-1 text-sm">
-          Edit
-        </button>
+        {isEditing ? (
+          <button onClick={handleUpdateAnime} className="bg-gray-950 text-white px-4 py-1 text-sm">
+            Save
+          </button>
+        ) : (
+          <button onClick={() => setIsEditing(true)} className="bg-gray-950 text-white px-4 py-1 text-sm">
+            Edit
+          </button>
+        )}
 
         <button onClick={handleDelete} className="bg-red-600 text-white px-4 py-1 text-sm">
           Delete
